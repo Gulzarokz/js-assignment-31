d00a608c8953b091268173bb5c1da5bb4471c272
Gulzarokz <gokz22301@gamil.com>
2024-10-19 18:45:43 +0500

    add and done exercise of browase controls

---
+++ index.js
@@ -0,0 +1,47 @@
+// // chapter no 76;
+// var currentURL = window.location.href;
+// console.log(currentURL);
+
+// extract some parts form url
+
+// // host name 
+// var DominName = window.location.hostname;
+// console.log(DominName);
+
+// var pathName = window.location.pathname;
+// console.log(pathName);
+
+// var ancharHash = window.location.hash;
+// console.log(ancharHash);
+
+
+// Window.locatio = "https://github.com/Gulzarokz/js-assignment-31";
+
+// chapterNo 77 getting and settig URL another way;
+
+ //Assign Methods
+//  var accessToGogole = window.location.assign("http://www.google.com");
+//  console.log(accessToGogole);
+ 
+//  var accessToYoubube = window.location.assign("http://www.youtube.com");
+//  console.log(accessToYoubube);
+
+// Replace Methods
+// var accessToGogole = window.location.assign("http://www.google.com");
+//  console.log(accessToGogole);
+ 
+// var accessToYoubube= window.location.replace("http://www.youtube.com");
+//   console.log(accessToYoubube);
+
+
+
+// Chapter NO => 78 Browser control: Forward and reversend reverse
+
+// go back history
+
+//  var histBack = history.back();
+//  console.log(histBack);
+ 
+
+// history.forward();
+ 
